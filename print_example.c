a5b1ec160f8eb40682da4d17933dd119b4756bca
gouthik <146082106+gouthik@users.noreply.github.com>
2023-10-03 14:32:27 +0300

    Class_07
    Upload trough GUI

---
+++ print_example.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+void main()
+{
+ int a,b;
+ char n;
+ printf("enter the name",n);
+ scanf n;
+ printf("enter the age",a);
+ scanf a;
+ printf("enter date of birth",b);
+ scanf b;
+ printf(a \n a \n b);
+ 
+ return 0;
+ }
+ 
+
