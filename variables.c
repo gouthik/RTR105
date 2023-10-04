bbf2846b488a7af644807efd8df47219ea465e76
gouthik <146082106+gouthik@users.noreply.github.com>
2023-10-04 12:03:24 +0300

    Add files via upload

---
+++ variables.c
@@ -0,0 +1,23 @@
+// let investigate data  types and variables
+#include<stdio.h>
+int main(void)
+{
+char c, test_symbol;
+printf("value of c variable as symbol:%c\n",c);
+printf("value of c variable as integer number (dec):%d\n",c);
+printf("value of c variable as integer number (oct):%o\n",c);
+printf("value of c variable as integer number (hex):%x\n",c);
+printf("\n");
+char c_new = 85;
+printf("value of c_new variable as symbol:%c\n",c_new);
+printf("value of c_new variable as integer number (dec):%d\n",c_new);
+printf("value of c_new variable as integer number (oct):%o\n",c_new);
+printf("value of c_new variable as integer number (hex):%x\n",c_new);
+c='y';
+printf("value of c  variable as symbol:%c\n",c);
+printf("value of c  variable as integer number (dec):%d\n",c_new);
+printf("value of c  variable as integer number (oct):%o\n",c_new);
+printf("value of c_new variable as integer number (hex):%x\n",c_new);
+
+return 0;
+}
