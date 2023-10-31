27f5740c055f881d52298a97eaf51abcf0f5171b
gouthik <146082106+gouthik@users.noreply.github.com>
2023-10-31 17:49:37 +0200

    Add files via upload

---
+++ Multiplication.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+ 
+ int main()
+ {
+ unsigned long var1,var2,result;
+ printf("enter First number:");
+ scanf("%d", &var1);
+ printf("enter Second number:");
+ scanf("%d", &var2);
+ result=var1*var2;
+ printf("Result: %d \n", result);
+ return 0;
+ }
